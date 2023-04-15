90e68dc24e213796e376e78b94d1f25901f6be83
seasaw-game <kasugaka-suga@outlook.com>
2023-04-15 23:50:24 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# h2
+# o2
+# h2o
+# ch4
+# c2h6
+# c3h8
+# c4h10
+
+# density[kg/m3]
+# Thermal Conductivity[W/m-k]
+# specific heat[J/kg-s]
+# viscosity[Pa-s]
+
+
+def property_calc():
+
