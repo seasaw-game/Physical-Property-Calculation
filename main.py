5a58aeb828a347a816b7021c47c60d8d51ba36cc
seasaw-game <kasugaka-suga@outlook.com>
2023-04-16 18:08:15 +0900

    add input widgets

--- main.py
+++ main.py
@@ -3,30 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-### composition
-# h2
-# o2
-# n2
-# h2o
-# ch4
-# c2h6
-# c3h8
-# c4h10
+### Composition
+COMPOSE_LIST = ["h2","o2","n2","h2o","ch4","c2h6","c3h8","c4h10"]
 
-### property
-# density[kg/m3]
+### Property
+# Density[kg/m3]
 # Thermal Conductivity[W/m-k]
-# specific heat[J/kg-s]
-# viscosity[Pa-s]
+# Specific Heat[J/kg-s]
+# Viscosity[Pa-s]
+PROPERTY_LIST = ["density","thermal_conductivity","specific_heat","viscosity"]
 
-# volume fraction
-volf = []
-volf[0] = st.number_input("H2")
-volf[1] = st.number_input("O2")
-volf[2] = st.number_input("H2O")
-volf[3] = st.number_input("CH4")
-volf[4] = st.number_input("C2H6")
-volf[5] = st.number_input("C3H8")
-volf[6] = 100 - volf.sum()
-st.text("N2")
-st.text(volf[6])
+# input volume fraction
+zero_data = np.zeros(shape=(1,len(COMPOSE_LIST)))
+volf = pd.DataFrame(zero_data, columns=COMPOSE_LIST)
+
+for i in COMPOSE_LIST:
+    volf[i] = st.number_input(i)
+
+total = volf.sum(axis=1)*100
+st.text(f"Total={total}%")
+
+st.text("ガス組成")
+volf
+
+# setting property
+col1, col2, col3 = st.columns(3)
+with col1:
+    temp_min = st.slider("最低温度", min_value=0, max_value=2000, value=0, step=100)
+with col2:
+    temp_max = st.slider("最高温度", min_value=500, max_value=2000, value=2000, step=100)
+with col3:
+    temp_interval = st.slider("出力点数", min_value=int((temp_max-temp_min)/100+1), max_value=int((temp_max-temp_min)/100)*2+1, value=int((temp_max-temp_min)/100+1), step=int((temp_max-temp_min)/100+1)-1)
+
+temp_list = np.linspace(temp_min, temp_max, temp_interval)
+thermophysic_list = pd.DataFrame(temp_list, columns=["Temperature[C]"])
+thermophysic_list["Temperature[K]"] = temp_list+273.15
+thermophysic_list
+
+for i in PROPERTY_LIST:
+    st.text(f"{i}")
